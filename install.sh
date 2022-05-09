d956af6ae9c49110509c37de8b9d733d4d60ff5a
heikki <sulinheikki@gmail.com>
2022-05-09 23:12:21 +0300

    Tweaks

--- install.sh
+++ install.sh
@@ -2,17 +2,19 @@
 
 # Install some good packs and configs #
 
+# PACKS #
+###
 # Paru for AUR packages and general managment + config file
 sudo pacman -S paru
 cp .config/paru/paru.conf $HOME/.config/paru.conf
 
-
-# OTHER PACKS #
+# Other packs
 paru -S mpv picom alacritty lf perl-file-mimeinfo librewolf-bin flameshot btop micro pcmanfm sxiv ueberzug bat xclip yt-dlp lsd xbanish python-pywal hsetroot dmenu
+###
 
 
 # PACK CONFIGS #
-
+###
 # MPV
 mkdir $HOME/.config/mpv
 cp .config/mpv/* $HOME/.config/mpv/
@@ -27,25 +29,25 @@ cp .config/lf/* $HOME/.config/lf/
 
 # Config mimeapps.list
 cp .config/mimeapps.list $HOME/.config/mimeapps.list
+###
 
 
 # Other personal config files #
+###
 cp .bash_profile $HOME/.bash_profile
 cp .bashrc $HOME/.bashrc
 cp .gtkrc-2.0 $HOME/.gtkrc-2.0
 cp .xinitrc $HOME/.xinitrc
 cp -r .themes $HOME/.themes
-mkdir $HOME/.local/share/fonts
+mkdir -p $HOME/.local/share/fonts
 cp fonts/* $HOME/.local/share/fonts
 cp -r .config/neofetch $HOME/.config/neofetch
 cp -r .config/gtk-3.0 $HOME/.config/gtk-3.0
-
-
-# BINS #
-sudo cp localbin/* /usr/local/bin/
+###
 
 
 # DWM SLSTATUS #
+###
 git clone https://github.com/JesusOfCode/mydwm
 cd mydwm
 sudo make clean install
@@ -54,7 +56,11 @@ git clone https://github.com/JesusOfCode/myslstatus
 cd myslstatus
 sudo make clean install
 cd ..
+###
 
 
 # BRUH #
 cp egg1.jpg $HOME
+
+# BINS #
+sudo cp localbin/* /usr/local/bin/
